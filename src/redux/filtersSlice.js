a62ebb5116669bf73455a95c1ea0aa274d6585a4
sauergru <sauergru@gmail.com>
2024-06-15 18:31:04 +0200

    added persist

--- src/redux/filtersSlice.js
+++ src/redux/filtersSlice.js
@@ -10,6 +10,6 @@ const filtersSlice = createSlice({
   },
 });
 
-export const filterReducer = filtersSlice.reducer;
+export const filtersReducer = filtersSlice.reducer;
 export const { changeFilter } = filtersSlice.actions;
 export const selectNameFilter = state => state.filters.name;
--- src/redux/rootReducer.js
+++
@@ -1,7 +0,0 @@
-import { contactsReducer } from './contactsSlice';
-import { filterReducer } from './filtersSlice';
-
-export const rootReducer = {
-  contacts: contactsReducer,
-  filters: filterReducer,
-};
